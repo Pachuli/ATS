2d1a5f8b6be389ab9190519938307220fa4e6341
Arturo Granados <arturo.granados@uteca.edu.mx>
2023-03-02 23:42:29 -0600

    Camera Move FIXED
    El segundo CANVAS impedía el movimiento de la cámara.  Como resultado se elimino de la escena debido a su nula necesidad para visualizar el mapa.

--- Assets/Scripts/MoveMap.cs
+++ Assets/Scripts/MoveMap.cs
@@ -5,37 +5,28 @@ using UnityEngine;
 public class MoveMap : MonoBehaviour
 {
 
-
-
-
-
-    //LA CAMARA NO PUEDE MOVERSE INDEPENDIENTEMENTE PORQUE ES EL PADRE DE TODOS LOS ELEMENTOS
-    //LOS CONTROLES NO FUNCIONAN, DEBO REFERENCIAR AL TRANSFORM DE LA CAMARA
-    //QUITAR LA SEGUNDA CAMARA DEL SEGUNDO CANVAS PARA PODER MOVERSE LIBREMENTE
-
-
     public float speed;
 
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
             this.transform.Translate(speed * Time.deltaTime, 0, 0);
         }
 
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             this.transform.Translate(-speed * Time.deltaTime,0,0);
         }
 
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
             this.transform.Translate(0, speed * Time.deltaTime, 0);
         }
    
 
-;       if (Input.GetKeyDown(KeyCode.S))
+;       if (Input.GetKey(KeyCode.S))
         {
             this.transform.Translate(0, -speed * Time.deltaTime, 0);
         }
